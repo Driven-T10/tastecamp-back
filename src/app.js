f70219c2edb216eec54ebc99075accbaf3fd33be
leticia-chijo <leticia.chijo@gmail.com>
2023-04-05 21:44:39 -0300

    Seção: Params e variáveis globais

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+
+const app = express() // app do servidor
+app.use(cors())
+
+const receitas = [
+    {
+        id: 1,
+        titulo: "Pão com Ovo",
+        ingredientes: "Ovo e pão",
+        preparo: "Frite o ovo e coloque no pão"
+    },
+    {
+        id: 2,
+        titulo: "Mingau de Whey",
+        ingredientes: "Leite, Aveia e Whey",
+        preparo: "Mistura tudo na panela fervendo",
+    }
+]
+
+app.get("/receitas", (req, res) => {
+    res.send(receitas)
+})
+
+app.get("/receitas/:id", (req, res) => {
+    const { id } = req.params
+    // const id = req.params.id
+
+    const receita = receitas.find((item) => item.id === Number(id))
+    res.send(receita)
+})
+
+const PORT = 4000
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`)) // 3000 e 5999

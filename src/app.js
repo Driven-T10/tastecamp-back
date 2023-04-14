e644073a7d6fbd7ee6bb5e88351db172e20e30c7
leticia-chijo <leticia.chijo@gmail.com>
2023-04-13 21:43:24 -0300

    Seção: async/await e try/catch (Parte 2)

--- src/app.js
+++ src/app.js
@@ -12,33 +12,39 @@ app.use(express.json())
 dotenv.config()
 
 // Conexão com o banco de dados
-let db
+
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message))
+try {
+    await mongoClient.connect()
+    console.log("MongoDB conectado!")
+} catch (err) {
+    console.log(err.message)
+}
+const db = mongoClient.db()
 
 // Endpoints
-app.get("/receitas", (req, res) => {
-    db.collection("receitas").find().toArray()
-        .then(receitas => res.send(receitas))
-        .catch(err => res.status(500).send(err.message))
+app.get("/receitas", async (req, res) => {
+    try {
+        const receitas = await db.collection("receitas").find().toArray()
+        res.send(receitas)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 })
 
-app.get("/receitas/:id", (req, res) => {
+app.get("/receitas/:id", async (req, res) => {
     const { id } = req.params
 
-    db.collection("receitas").findOne({ _id: new ObjectId(id) })
-        .then((receita) => {
-            if (!receita) return res.status(404).send("Receita não existe")
-            res.send(receita)
-        })
-        .catch((err) => res.status(500).send(err.message))
+    try {
+        const receita = await db.collection("receitas").findOne({ _id: new ObjectId(id) })
+        if (!receita) return res.status(404).send("Receita não existe")
+        res.send(receita)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 })
 
-// função POST
-app.post("/receitas", (req, res) => {
-    // função de callback do POST
+app.post("/receitas", async (req, res) => {
     const { titulo, ingredientes, preparo } = req.body
 
     if (!titulo || !ingredientes || !preparo) {
@@ -47,20 +53,16 @@ app.post("/receitas", (req, res) => {
 
     const novaReceita = { titulo, ingredientes, preparo }
 
-    db.collection("receitas").findOne({ titulo: titulo })
-        // sucesso da requisição findOne
-        .then((data) => {
-            if (data) {
-                return res.status(409).send("Essa receita já existe!! Escolha outro título.")
-            } else {
-                db.collection("receitas").insertOne(novaReceita)
-                    .then(() => res.sendStatus(201)) // sucesso do insertOne
-                    .catch(err => res.status(500).send(err.message)) // erro do insertOne
-            }
-        })
-        .catch(err => res.status(500).send(err.message)) // erro da findOne
-})
+    try {
+        const recipe = await db.collection("receitas").findOne({ titulo: titulo })
+        if (recipe) return res.status(409).send("Essa receita já existe!")
 
+        await db.collection("receitas").insertOne(novaReceita)
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
 
 // Deixa o app escutando, à espera de requisições
 const PORT = 4000

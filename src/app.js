7654d7cdaf630aead938d8029b2ff0beadfa4919
leticia-chijo <leticia.chijo@gmail.com>
2023-04-18 21:53:01 -0300

    Seção: sign up e bcrypt

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
+import bcrypt from "bcrypt"
 
 // Criação do App Servidor
 const app = express()
@@ -13,7 +14,6 @@ app.use(express.json())
 dotenv.config()
 
 // Conexão com o banco de dados
-
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
 try {
     await mongoClient.connect()
@@ -22,6 +22,13 @@ try {
     console.log(err.message)
 }
 const db = mongoClient.db()
+
+// Schemas
+const usuarioSchema = joi.object({
+    nome: joi.string().required(),
+    email: joi.string().email().required(),
+    senha: joi.string().required().min(3)
+})
 
 // Endpoints
 app.get("/receitas", async (req, res) => {
@@ -153,6 +160,45 @@ app.put("/receitas/muitas/:filtroIngredientes", async (req, res) => {
         if (result.matchedCount === 0) return res.status(404).send("Não há nenhuma receita com esse filtro!")
         res.send("Receitas editadas!")
 
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/sign-up", async (req, res) => {
+    const { nome, email, senha } = req.body
+
+    const validation = usuarioSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const usuario = await db.collection("usuarios").findOne({ email })
+        if (usuario) return res.status(409).send("E-mail já cadastrado")
+
+        const hash = bcrypt.hashSync(senha, 10)
+
+        await db.collection("usuarios").insertOne({ nome, email, senha: hash })
+        res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/sign-in", async (req, res) => {
+    const { email, senha } = req.body
+
+    try {
+        const usuario = await db.collection("usuarios").findOne({ email })
+        if (!usuario) return res.status(401).send("E-mail não cadastrado.")
+
+        const senhaEstaCorreta = bcrypt.compareSync(senha, usuario.senha)
+        if (!senhaEstaCorreta) return res.status(401).send("Senha incorreta")
+
+        res.sendStatus(200)
     } catch (err) {
         res.status(500).send(err.message)
     }
